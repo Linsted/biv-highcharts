67ef794ffa6c91ab9a0b9935cdef360dfc99475c
Ivan Malenko <109988731+Linsted@users.noreply.github.com>
2025-06-19 13:13:28 +0200

    feat: 30 days filter as default

--- src/pages/MyDashboardPage.tsx
+++ src/pages/MyDashboardPage.tsx
@@ -7,7 +7,7 @@ import { TimePeriodSelector } from "../components/filters/TimePeriodSelector";
 import { useVisualizationData } from "../hooks/data-adapters/useVisualizationData";
 
 export const MyDashboardPage = () => {
-  const [timePeriod, setTimePeriod] = useState("LAST_7_DAYS");
+  const [timePeriod, setTimePeriod] = useState("LAST_30_DAYS");
 
   const salesQuery = useVisualizationData({ metricId: "sales", timePeriod });
   const usersQuery = useVisualizationData({ metricId: "users", timePeriod });
